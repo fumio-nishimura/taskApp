cd3452855b1b272208211dbc69623f70be441c2b
fumio-nishimura <cityhunter0522@gmail.com>
2020-08-07 10:35:56 +0900

    Initial Commit

---
+++ taskApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  taskApp
+//
+//  Created by 西村史旺 on 2020/08/07.
+//  Copyright © 2020 Fumio Nishimura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
