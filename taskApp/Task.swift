ca4b5db8b98f143cf9f74181e21e06b563c4ab67
fumio-nishimura <cityhunter0522@gmail.com>
2020-08-09 19:56:54 +0900

    タスク管理アプリを作成した

---
+++ taskApp/Task.swift
@@ -0,0 +1,25 @@
+//
+//  Task.swift
+//  taskApp
+//
+//  Created by 西村史旺 on 2020/08/08.
+//  Copyright © 2020 Fumio Nishimura. All rights reserved.
+//
+
+import RealmSwift
+
+class Task: Object {
+    // 管理用 ID プライマリーキー
+    @objc dynamic var id = 0
+    // タイトル
+    @objc dynamic var title = ""
+    // 内容
+    @objc dynamic var contents = ""
+    // 日時
+    @objc dynamic var date = Date()
+
+    // id をプライマリーキーとして設定
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
